11bed79c270a83db32cf2098de74b938395b65cb
Bumbea <bumbeaa@amazon.com>
2023-01-20 16:31:58 +0200

    first commit

---
+++ src/components/TerminationLetter.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import { useState } from "react";
+import jsPDF from "jspdf";
+
+const ResignationLetter = () => {
+  const [letter, setLetter] = useState({
+    recipient: "",
+    sender: "",
+    location: "",
+    series: "",
+    number: "",
+    position: "",
+    date: "",
+    reason: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setLetter({
+      ...letter,
+      [name]: value,
+    });
+  };
+
+  const downloadPDF = () => {
+    const doc = new jsPDF('portrait');
+    const {
+      recipient,
+      sender,
+      date,
+      reason,
+      series,
+      number,
+      position,
+      location,
+    } = letter;
+    doc.text("Catre: AMAZON DEVELOPMENT CENTER (ROMANIA) S.R.L.", 10, 10);
+    doc.text(`Domnule Administrator,`, 10, 15);
+    doc.text(`Subsemnata ${sender}, domiciliata in ${location}, identificata prin seria ${series}, nr. ${number}, angajata in functia de ${position},`,10,20);
+    doc.text(`in cadrul AMAZON DEVELOPMENT CENTER(ROMANIA) S.R.L.,`, 10, 25);
+    doc.text(`cu sediul in Iasi, str. Sf.Lazar nr.27A, Cladirea United Business Center 5, et.2, Jud.Iasi, inregistrata in Registrul Comertului Iasi`,10,30);
+    doc.text(`sub nr.J22/2621/2005, avand CUI 17810435, va`, 10,35);
+    doc.text(`rog sa aprobati cererea de incetare a contractului individual de munca, cu acordul partilor, in temeiul prevederilor art. 55,`,10,40);
+    doc.text(`lit. b, din Legea nr.53/2003 - Codul Muncii, inceput cu data ${date}`,10,45);
+    doc.text(`Sincerely,`, 10, 50);
+    doc.text(`${sender}`, 20, 60);
+    doc.save("resignation_letter.pdf");
+  };
+
+  return (
+    <div>
+      <form>
+        <label>
+          Sender:
+          <input type="text" name="sender" onChange={handleChange} />
+        </label>
+        <label>
+          Location:
+          <input type="text" name="Location" onChange={handleChange} />
+        </label>
+        <label>
+          Series:
+          <input type="text" name="Seria" onChange={handleChange} />
+        </label>
+        <label>
+          Number:
+          <input type="text" name="Numar" onChange={handleChange} />
+        </label>
+        <label>
+          Position:
+          <input type="text" name="Pozitia" onChange={handleChange} />
+        </label>
+        <label>
+          Date:
+          <input type="text" name="date" onChange={handleChange} />
+        </label>
+      </form>
+      <button onClick={downloadPDF}>Download as PDF</button>
+    </div>
+  );
+};
+
+export default ResignationLetter;
